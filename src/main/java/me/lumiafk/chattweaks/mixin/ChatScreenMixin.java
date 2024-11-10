ddbfa399744dc71ffbcfe8f1e2b9a4ea905a1362
Rime <81419447+Emirlol@users.noreply.github.com>
2024-11-11 02:59:33 +0300

    Oops forgot to commit the rest of ce1e839678f1b45ba8677998b5028f9af152e43d

--- src/main/java/me/lumiafk/chattweaks/mixin/ChatScreenMixin.java
+++ src/main/java/me/lumiafk/chattweaks/mixin/ChatScreenMixin.java
@@ -1,20 +1,15 @@
 package me.lumiafk.chattweaks.mixin;
 
+import me.lumiafk.chattweaks.config.ConfigHandler;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ChatScreen.class)
-public class ChatScreenMixin extends Screen {
-	protected ChatScreenMixin(Text text) {
-		super(text);
-	}
-
+public class ChatScreenMixin {
 	@ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setMaxLength(I)V"))
-	private int modifyChatHeight(int height) {
-		return 10000;
+	private int chatTweaks$modifyChatWidth(int i) {
+		return ConfigHandler.INSTANCE.getConfig().otherConfig.chatWidth;
 	}
 }
