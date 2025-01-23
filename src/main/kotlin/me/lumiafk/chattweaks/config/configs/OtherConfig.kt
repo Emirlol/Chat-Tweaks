932c4e42ae16115aae7f7ccd8591f54484101c73
Rime <81419447+Emirlol@users.noreply.github.com>
2025-01-23 05:21:59 +0300

    Remove the extra default chat boxes since they are configurable now

--- src/main/kotlin/me/lumiafk/chattweaks/config/configs/OtherConfig.kt
+++ src/main/kotlin/me/lumiafk/chattweaks/config/configs/OtherConfig.kt
@@ -6,13 +6,8 @@ import dev.isxander.yacl3.config.v3.ConfigEntry
 import dev.isxander.yacl3.config.v3.register
 import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import me.lumiafk.chattweaks.chat.ChatBox
-import me.lumiafk.chattweaks.chat.ChatBoxData
-import me.lumiafk.chattweaks.chat.transformations.Transformer
-import me.lumiafk.chattweaks.chat.transformations.TransformerContext
-import me.lumiafk.chattweaks.chat.transformations.TransformerType
 import me.lumiafk.chattweaks.config.ConfigProxy
 import me.lumiafk.chattweaks.util.Codecs.mutableList
-import me.lumiafk.chattweaks.util.text
 
 object OtherConfig : ConfigProxy {
 	val chatWidth by register(1024)
@@ -21,7 +16,5 @@ object OtherConfig : ConfigProxy {
 
 	val chatBoxes: ConfigEntry<MutableList<ChatBox>> by register(ObjectArrayList<ChatBox>().apply {
 		add(ChatBox.DEFAULT_CHAT_BOX)
-		add(ChatBox("Hi".text, mutableListOf(Transformer(TransformerType.STRING_REPLACE, TransformerContext(stringFilter = "potato", replacement = "carrot"))), ChatBoxData.DEFAULT.clone().apply { y += 200 }))
-		add(ChatBox("Hi".text, mutableListOf(Transformer(TransformerType.STRING_REPLACE, TransformerContext(stringFilter = "potato", replacement = "hi there"))), ChatBoxData.DEFAULT.clone().apply { y += 400 }))
 	}, ChatBox.CODEC.mutableList())
 }
